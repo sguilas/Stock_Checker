883f8991054cadebb8ef227400fdd6927ec547d3
Stefan Guilas <92962458+sguilas@users.noreply.github.com>
2022-01-20 17:44:16 -0500

    Update stock_checker.py

--- stock_checker.py
+++ stock_checker.py
@@ -30,45 +30,36 @@ def all(user, url):
 
 print(f"This checks the PS5 and Xbox Series Product pages whether they are in stock by parsing through the HTMl.\n")
 print(f"If it's in stock it will open the webpage so you could buy it and stop the loop.\n")
-
 user = user()
-ps5 = "https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149"
-ps5_dig = "https://www.bestbuy.com/site/sony-playstation-5-digital-edition-console/6430161.p?skuId=6430161"
-xbox_x = "https://www.bestbuy.com/site/microsoft-xbox-series-x-1tb-console-black/6428324.p?skuId=6428324"
-xbox_s = "https://www.bestbuy.com/site/microsoft-xbox-series-s-512-gb-all-digital-console-disc-free-gaming-white/6430277.p?skuId=6430277"
+urls = {}
 often = 15
+flag = True
+times = int(input("Enter how many URLs you will track: "))
+i=0
+while times!=0:
+    prod_name = str(input("What you are tracking: "))
+    prod_url = str(input("URL: "))
+    urls[prod_name] = prod_url
+    times-=1
 
-while True:
+#user = user()
+#ps5 = "https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149"
+#ps5_dig = "https://www.bestbuy.com/site/sony-playstation-5-digital-edition-console/6430161.p?skuId=6430161"
+#xbox_x = "https://www.bestbuy.com/site/microsoft-xbox-series-x-1tb-console-black/6428324.p?skuId=6428324"
+#xbox_s = "https://www.bestbuy.com/site/microsoft-xbox-series-s-512-gb-all-digital-console-disc-free-gaming-white/6430277.p?skuId=6430277"
+
+while flag == True:
     now = datetime.datetime.now()
     real_time = now.strftime("%d/%m/%Y %H:%M:%S")
-    print("################################################")
-    print("For PS5 (Best Buy):")
-    ans_ps5 = all(user,ps5)
-    if ans_ps5==True:
-        webbrowser.open(ps5)
-        break
-    else:
-        print(f"It's still sold out as of {real_time}\n")
-    print("For PS5 (Digital) (Best Buy):")
-    ans_ps5D = all(user,ps5_dig)
-    if ans_ps5D==True:
-        webbrowser.open(ps5_dig)
-        break
-    else:
-        print(f"It's still sold out as of {real_time}\n")
-    print("For Xbox Series X (Best Buy):")
-    ans_Xbx = all(user,xbox_x)
-    if ans_Xbx==True:
-        webbrowser.open(xbox_x)
-        break
-    else:
-        print(f"It's still sold out as of {real_time}\n")
-    print("For Xbox Series S (Best Buy):")
-    ans_Xbs = all(user,xbox_s)
-    if ans_Xbs==True:
-        webbrowser.open(xbox_s)
-        break
-    else:
-        print(f"It's still sold out as of {real_time}\n")
+    for key, url in urls.items():
         print("################################################")
+        print(f"For {key} (Best Buy):")
+        ans = all(user,url)
+        if ans == True:
+            webbrowser.open(url)
+            flag = False
+            break
+        else:
+            print(f"It's still sold out as of {real_time}\n")
+
     time.sleep(often)
